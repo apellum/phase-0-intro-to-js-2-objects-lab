d06d12277c26c85ae189cce3b099d27468b3044c
Andrew Pellum <apellum19@gmail.com>
2021-06-10 16:37:51 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-let employee = {
+const employee = {name: '', streetAddress: ''}
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return Object.assign({}, employee, {[key]: value})
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
+    return employee
+}
+
+const deleteFromEmployeeByKey = (employee, key, value) => {
+    const newEmployee = Object.assign({}, employee)
+    delete newEmployee[key]
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let employee = {
     name: ``,
     streetAddress: ``,
 }
@@ -18,4 +77,4 @@ function deleteFromEmployeeByKey(employee, key) {
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key]
     return employee
-}
+}*/
